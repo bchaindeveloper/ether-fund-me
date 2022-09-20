e2e649f3dd58ad1c29f1906324e5c3116e2dafca
bchaindeveloper <bcdev3@protonmail.com>
2022-09-20 11:47:47 -0400

    Get Balance

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const withdrawButton = document.getElementById("withdrawButton")
 const fundButton = document.getElementById("fundButton")
 const balanceButton = document.getElementById("balanceButton")
 connectButton.onclick = connect
-// withdrawButton.onclick = withdraw
+withdrawButton.onclick = withdraw
 fundButton.onclick = fund
 balanceButton.onclick = getBalance
 
@@ -25,24 +25,24 @@ async function connect() {
     }
 }
 
-// async function withdraw() {
-//     console.log(`Withdrawing...`)
-//     if (typeof window.ethereum !== "undefined") {
-//         const provider = new ethers.providers.Web3Provider(window.ethereum)
-//         await provider.send("eth_requestAccounts", [])
-//         const signer = provider.getSigner()
-//         const contract = new ethers.Contract(contractAddress, abi, signer)
-//         try {
-//             const transactionResponse = await contract.withdraw()
-//             await listenForTransactionMine(transactionResponse, provider)
-//             // await transactionResponse.wait(1)
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     } else {
-//         withdrawButton.innerHTML = "Please install MetaMask"
-//     }
-// }
+async function withdraw() {
+    console.log(`Withdrawing...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+            // await transactionResponse.wait(1)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = "Please install MetaMask"
+    }
+}
 
 async function fund() {
     const ethAmount = document.getElementById("ethAmount").value
