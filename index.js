9f8883fa8a8b09a13359b8f569d7c65a10223cb4
bchaindeveloper <bcdev3@protonmail.com>
2022-09-20 11:04:29 -0400

    Constants

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi } from "./constants"
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
 
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    try {
-      await ethereum.request({ method: "eth_requestAccounts" });
-    } catch (error) {
-      console.log(error);
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
+    } else {
+        connectButton.innerHTML = "Please install MetaMask"
     }
-    connectButton.innerHTML = "Connected";
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-    console.log(accounts);
-  } else {
-    connectButton.innerHTML = "Please install MetaMask";
-  }
+}
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provide = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = " "
+    }
 }
